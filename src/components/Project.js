e7e4910b8787551aeadb68f42d65ee76fc4119f0
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-04 13:08:26 +0000

    Project gallery displays each project by mapping through json data

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,3 +6,20 @@ import React from 'react';
 
 //TODO pin the repositories on GitHub profile
 
+function Project(props) {
+    return (
+        <div>
+            <div className="img-container">
+                <img alt={`Screenshot of ${props.title}`} src={props.image} />
+            </div>
+            <div className="project-content">
+                <h2>{props.title}</h2>
+                <p>{props.description}</p>
+                <a href={props.deployed}>Deployed App</a> / 
+                <a href={props.repo}>GitHub Repo</a>
+            </div>
+        </div>
+    )
+}
+
+export default Project;
--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Project from './Project';
+import projects from '../projects.json';
 
 //TODO must render dynamically 6 instances of the Project component
 
 function ProjectGallery() {
+  const [project, setProject]= useState(projects)
+  console.log();
     return (
       <div>
-        <h1>My Projects</h1>
+        {project.map(project => (
+        <Project 
+        title={project.title}
+        image={project.image}
+        description={project.description}
+        deployed={project.deployed}
+        repo={project.repo}
+        key={project.key}
+        />
+        ))}
       </div>
     );
 }
