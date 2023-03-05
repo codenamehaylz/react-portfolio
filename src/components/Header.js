66f79e7df9cd02fad868938c78fa7bf95b7dd0b2
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-05 22:34:30 +0000

    added bootstrap classes to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-//TODO add styling and classes
-
 function Header() {
     return (
-        <nav>
-            <NavLink 
-            to="/"
-            className="nav-link name">
-                Hayley Blackburn
-            </NavLink>
-            
-            <ul>
-                <li>
+        <nav className="navbar">
+            <div className="container-fluid">
+                <NavLink 
+                    to="/"
+                    className="nav-link navbar-brand">
+                        Hayley Blackburn
+                    </NavLink>
+
+            <ul className="nav">
+                <li className="nav-item">
                     <NavLink 
                     to="/projects"
                     className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
@@ -21,13 +20,15 @@ function Header() {
                     </NavLink>
                 </li>
 
-                <li>
+                <li className="nav-item">
                     <NavLink to="/contact"
                     className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
                         Contact
                     </NavLink>
                 </li>
             </ul>
+
+            </div>
         </nav>
     )
 }
