bbe46a6233acb3951c45a459a267a2653c3c5446
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-05 12:40:18 +0000

    contact form is functional and sends message to my email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,44 @@ import { useState } from 'react';
 //TODO contact form for handling events
 
 function Contact() {
+  const [success, setSuccess] = useState(false);
+
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
-    message: ""
+    email: "",
+    message: "",
+    access_key: "0eabe439-a824-4e37-9720-c804be54627a"
   });
 
   const formSubmit = (event) => {
     event.preventDefault()
-    console.log(formData)
+    const json = JSON.stringify(formData);
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+      })
+      .then(response => response.json())
+      .then(data => {
+        setSuccess(true);
+        setFormData({
+          ...formData,
+          firstName: "",
+          lastName: "",
+          email: "",
+          message: "",
+        })
+        setTimeout(() => {
+          setSuccess(false);
+        }, 4000);
+      })
+      .catch(error => console.log(error));
+
   }
 
   const formChange = (event) => {
@@ -29,24 +58,42 @@ function Contact() {
         <h1>Contact Me</h1>
 
         <form onSubmit={formSubmit}>
+
+          <input 
+            type="hidden" 
+            name="access_key" 
+            value="0eabe439-a824-4e37-9720-c804be54627a" />
+
           <input
             onChange={formChange}
             type="text" 
             name="firstName"
-            placeholder="First Name"/>
+            value={formData.firstName}
+            placeholder="First Name" />
 
           <input
             onChange={formChange}
             type="text" 
             name="lastName"
-            placeholder="Last Name"/>
+            value={formData.lastName}
+            placeholder="Last Name" />
+
+          <input
+            onChange={formChange}
+            type="text" 
+            name="email"
+            value={formData.email}
+            placeholder="Email address" />
 
           <textarea
             onChange={formChange}
             name="message"
+            value={formData.message}
             placeholder="Type your message here" />
 
-          <button>Submit</button>
+          <button type="submit">Submit</button>
+
+          {success && <p>Message Sent!</p>}
         </form>
       </div>
     );
