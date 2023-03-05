b2f34b9614ad90a4f0b654752d607abe7fc2ea84
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-05 23:40:27 +0000

    responsive font size of projects title to match contacts title

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react';
 import Project from './Project';
 import projects from '../projects.json';
 
-//TODO display projects in grid
-
 function ProjectGallery() {
   //sets the 'project' state to all the projects in my json file
   const [project, setProject] = useState(projects)
     return (
       <div>
-        <h1 className="text-center p-3">Projects</h1>
+        <h1 className="project-title text-center p-3">Projects</h1>
         <div className="project-container container">
           <div className="row row-cols-sm-1 row-cols-lg-2 g-4">
             {/* iterates through the projects and displays the info for each one*/}
