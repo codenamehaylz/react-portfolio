a22d3f438ec08b45c730dcbc017d7c1054de66dd
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-05 14:39:02 +0000

    added content to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
-//TODO welcome landing page containing: Name, Headshot, brand statement, some indiciation this is a portfolio site
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Home() {
     return (
       <div>
-        <h1>Home</h1>
+        <div className="hero-container">
+          <img alt="Headshot of Hayley Blackburn" src={require("../images/Me.jpg")} />
+          <h1>Hayley Blackburn</h1>
+          <h2>Front End Web Developer</h2>
+          <p>Welcome to my portfolio! Go ahead and take a look at the previous projects that I've worked on. My passion is creating beautiful products with clean and efficient code.</p>
+          <a href="https://www.linkedin.com/in/hayley-blackburn1/"><FontAwesomeIcon icon={faLinkedin} /></a> 
+          <a href="https://github.com/codenamehaylz"><FontAwesomeIcon icon={faGithub} /></a>
+        </div>
       </div>
     );
 }
